abefc9598c467f6724e5693f634997640895eb19
Emily Söhler <es@gpredictive.de>
2016-11-21 18:26:07 +0100

    added dropping columns + dependency

--- flightDelay/src/main/scala/eit/App.scala
+++ flightDelay/src/main/scala/eit/App.scala
@@ -3,7 +3,9 @@ package eit
 /**
  * @author ${user.name}
  */
+
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.log4j.{Level, Logger}
@@ -12,9 +14,27 @@ object App {
     Logger.getRootLogger().setLevel(Level.WARN)
     val conf = new SparkConf().setAppName("My first Spark application").setMaster("local[1]")
     val sc = new SparkContext(conf)
-    val data = sc.textFile(args(0))
-    val numAs = data.filter(line => line.contains("a")).count()
-    val numBs = data.filter(line => line.contains("A")).count()
-    println(s"Lines with a: ${numAs}, Lines with b: ${numBs}")
+
+    val spark = SparkSession
+      .builder()
+      .appName("BigData")
+      .getOrCreate()
+
+    val data = spark.read.format("org.apache.spark.csv").option("header", "true").csv(args(0))
+
+
+    val relevant = data.drop(
+      "ArrTime",
+      "ActualElapsedTime",
+      "AirTime",
+      "TaxiIn",
+      "Diverted",
+      "CarrierDelay",
+      "WeatherDelay",
+      "NASDelay",
+      "SecurityDelay",
+      "LateAircraftDelay"
+    )
+    println("Hello" + relevant.columns)
   }
 }
