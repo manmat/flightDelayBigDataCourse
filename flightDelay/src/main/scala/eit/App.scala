92ba79e77bde098aba100fbd444a6a9092f2fe8e
Mani <manninger.matyas@gmail.com>
2016-11-21 17:05:29 +0100

    set up spark

---
+++ flightDelay/src/main/scala/eit/App.scala
@@ -0,0 +1,20 @@
+package eit
+
+/**
+ * @author ${user.name}
+ */
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.log4j.{Level, Logger}
+object App {
+  def main(args : Array[String]) {
+    Logger.getRootLogger().setLevel(Level.WARN)
+    val conf = new SparkConf().setAppName("My first Spark application").setMaster("local[1]")
+    val sc = new SparkContext(conf)
+    val data = sc.textFile(args(0))
+    val numAs = data.filter(line => line.contains("a")).count()
+    val numBs = data.filter(line => line.contains("b")).count()
+    println(s"Lines with a: ${numAs}, Lines with b: ${numBs}")
+  }
+}
