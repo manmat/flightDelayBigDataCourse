d3e953cc29a5f39f473578f3de9d168acd84cf40
Emily Söhler <es@gpredictive.de>
2016-11-27 14:07:51 +0100

    changed spark session variables and read

--- flightDelay/src/main/scala/eit/App.scala
+++ flightDelay/src/main/scala/eit/App.scala
@@ -7,21 +7,35 @@ package eit
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkContext._
 import org.apache.log4j.{Level, Logger}
 object App {
   def main(args : Array[String]) {
     Logger.getRootLogger().setLevel(Level.WARN)
-    val conf = new SparkConf().setAppName("My first Spark application").setMaster("local[1]")
+
+    val conf = new SparkConf().setAppName("MyFirstSparkApplication").setMaster("local[1]")
     val sc = new SparkContext(conf)
 
     val spark = SparkSession
       .builder()
-      .appName("BigData")
+      .appName("MyFirstSparkApplication")
+      .enableHiveSupport()
       .getOrCreate()
 
-    val data = spark.read.format("org.apache.spark.csv").option("header", "true").csv(args(0))
+    // val sq = SQLContext()
 
+    val data = spark
+      .read
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .option("sep",";")
+      .option("mode", "DROPMALFORMED")
+      .load(args(0))
+
+
+      // spark.read.format("org.apache.spark.csv").option("header", "true").csv(args(0))
+    // path should be preferably hdfs path
 
     val relevant = data.drop(
       "ArrTime",
@@ -35,6 +49,12 @@ object App {
       "SecurityDelay",
       "LateAircraftDelay"
     )
+
+    val target = "ArrDelay"
+    // transform variables for linear regression
+    // put into form (target, (other variables))
+    // create precision and recall as well as ROC curve? other measures?
+
     println("Hello" + relevant.columns)
   }
 }
