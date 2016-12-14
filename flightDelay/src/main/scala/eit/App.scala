bd8c37581100bd2b997fb2921d1564419180c41d
Emily Söhler <es@gpredictive.de>
2016-12-14 14:09:13 +0100

    tidied up, included crossvalidator and other changes

--- flightDelay/src/main/scala/eit/App.scala
+++ flightDelay/src/main/scala/eit/App.scala
@@ -13,6 +13,10 @@ import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.ml.{Pipeline, PipelineStage}
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit, CrossValidator}
+import org.apache.spark.ml.evaluation.{RegressionEvaluator}
+import org.apache.spark.mllib.evaluation.RegressionMetrics
+import org.apache.spark.sql.Row
 import Array._
 
 
@@ -39,82 +43,114 @@ object App {
       .option("mode", "DROPMALFORMED")
       .load(args(0))
 
-    val relevant = data.drop(
-      "ArrTime",
-      "ActualElapsedTime",
-      "AirTime",
-      "TaxiIn",
-      "Diverted",
-      "CarrierDelay",
-      "WeatherDelay",
-      "NASDelay",
-      "SecurityDelay",
-      "LateAircraftDelay"
-    )
+
+    // add airport business for origin and destination
+    val size_airport_orig = data.groupBy("Origin").count()
+    val size_airport_dest = data.groupBy("Dest").count()
+
+    val joined = data.join(size_airport_orig, "Origin")
+      .withColumnRenamed("count", "AirportBusinessOrig")
+      .join(size_airport_dest, "Dest")
+      .withColumnRenamed("count", "AirportBusinessDest")
+
+
+    // select and convert types of Variables that should be used
+    // and filter out values where ArrDelay is null
+    val converted = joined.select(
+      joined("Month"),
+      joined("DayofMonth"),
+      joined("UniqueCarrier"),
+      joined("DayOfWeek"),
+      joined("DepDelay").cast(IntegerType),
+      joined("CRSDepTime").cast(IntegerType),
+      joined("ArrDelay").cast(DoubleType),
+      joined("Distance").cast(DoubleType),
+      joined("AirportBusinessDest").cast(DoubleType),
+      joined("AirportBusinessOrig").cast(DoubleType),
+      joined("CRSElapsedTime").cast(DoubleType),
+      joined("TaxiOut").cast(DoubleType)
+    ).where("ArrDelay is not null")
 
 
     // add vector columns for categorical variables
-
     val to_index = List("DayOfWeek", "Month", "UniqueCarrier", "DayofMonth")
 
     val indexers = to_index.map{i => new StringIndexer()
       .setInputCol(i)
       .setOutputCol(i + "Index")
-      .fit(relevant)
     }
-    val encoders = to_index.map{i => new OneHotEncoder()
-      .setInputCol(i + "Index")
-      .setOutputCol(i + "Vec")
+
+    val encoders = to_index.map{j => new OneHotEncoder()
+      .setInputCol(j + "Index")
+      .setOutputCol(j + "Vec")
     }
+
+    // assemble features as vector
+    val assembler = new VectorAssembler()
+      .setInputCols(
+        Array("MonthVec", "DayofMonthVec","UniqueCarrierVec","DayOfWeekVec",
+          "DepDelay", "CRSDepTime","Distance" , "AirportBusinessDest",
+          "AirportBusinessOrig", "CRSElapsedTime", "TaxiOut"))
+      .setOutputCol("features")
+
+    // build a linear model
+    val lr = new LinearRegression()
+      .setFeaturesCol("features")
+      .setLabelCol("ArrDelay")
+      .setMaxIter(10)
+      .setElasticNetParam(0.8)
+
+    // perform grid search
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(lr.regParam, Array(0.1, 0.01))
+      .addGrid(lr.fitIntercept)
+      .addGrid(lr.elasticNetParam, Array(0.0, 1.0))
+      .build()
+
+    val stages : Array[PipelineStage] = concat(indexers.toArray, encoders.toArray, Array(assembler, lr))
+
+    val pipeline = new Pipeline().setStages(stages)
+
+    val tvs = new TrainValidationSplit()
+      .setEstimator(pipeline)
+      .setEvaluator(new RegressionEvaluator().setLabelCol("ArrDelay"))
+      .setEstimatorParamMaps(paramGrid)
+      .setTrainRatio(0.7)
+
+    val cv = new CrossValidator()
+      .setEstimator(pipeline)
+      .setEvaluator(new RegressionEvaluator().setLabelCol("ArrDelay"))
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(3)
+
+
+    // split into train and test datasets
+    val Array(trainingData, testData) = converted.randomSplit(Array(0.7, 0.3), seed = 12546)
+
+    val model = tvs.fit(trainingData)
+    //val cvModel = cv.fit(trainingData)
+    val predictions = model.transform(testData)
+      .select("ArrDelay", "prediction")
+
+    val metrics = new RegressionMetrics(predictions.rdd.map(x =>
+      (x(0).asInstanceOf[Double], x(1).asInstanceOf[Double])))
+
+    println(s"RMSE: ${metrics.rootMeanSquaredError}")
+    println(s"MSE: ${metrics.meanSquaredError}")
+    println(s"r2: ${metrics.r2}")
+    println(s"Explained Variance: ${metrics.explainedVariance}")
+
+
+    /*
 
     val collection :Array[PipelineStage] = concat(indexers.toArray, encoders.toArray)
 
-    val pipeline_encode = new Pipeline()
-      .setStages(collection)
+    val pipeline_encode = new Pipeline().setStages(collection)
 
-    val encoded = pipeline_encode.fit(relevant).transform(relevant)
-
-
-    // add airport business for origin and destination
-    val size_airport_orig = encoded.groupBy("Origin").count()
-    val size_airport_dest = encoded.groupBy("Dest").count()
-
-    val joined = encoded.join(size_airport_orig, "Origin")
-      .withColumnRenamed("count", "AirportBusinessOrig")
-      .join(size_airport_dest, "Dest")
-      .withColumnRenamed("count", "AirportBusinessDest")
-
-    // drop forbidden columns
-    val converted = joined.select(
-      joined("MonthVec"),
-      joined("DayofMonthVec"),
-      joined("UniqueCarrierVec"),
-      joined("DayOfWeekVec"),
-      joined("DepDelay").cast(IntegerType),
-      joined("DepTime").cast(IntegerType),
-      joined("ArrDelay").cast(DoubleType),
-      joined("Distance").cast(DoubleType),
-      joined("AirportBusinessDest").cast(DoubleType),
-      joined("AirportBusinessOrig").cast(DoubleType),
-      joined("CRSElapsedTime").cast(DoubleType)
-    ).where("ArrDelay is not null")
+    val encoded = pipeline_encode.fit(converted).transform(converted)
 
 
-    val Array(trainingData, testData) = converted.randomSplit(Array(0.7, 0.3))
 
-    val assembler = new VectorAssembler()
-     .setInputCols(
-       Array("MonthVec", "DayofMonthVec","UniqueCarrierVec","DayOfWeekVec",
-         "DepDelay", "DepTime","Distance" , "AirportBusinessDest",
-         "AirportBusinessOrig", "CRSElapsedTime"))
-     .setOutputCol("features")
-
-
-    val lr = new LinearRegression()
-        .setFeaturesCol("features")
-        .setLabelCol("ArrDelay")
-        .setMaxIter(10)
-        .setElasticNetParam(0.8)
 
     val pipeline_model = new Pipeline()
       .setStages(Array(assembler, lr))
@@ -130,11 +166,13 @@ object App {
     val output = assembler.transform(trainingData)
     val lrModel2 = lr.fit(output)
     println("Model 1 was fit using parameters: " + lrModel2.parent.extractParamMap)
-
+    //Predicting the arrival delay of commercial flights, how to measure??
 
     // Print the coefficients and intercept for linear regression
     println(lrModel2.featuresCol)
-    println(s"Coefficients: ${lrModel2.coefficients} Intercept: ${lrModel2.intercept}")
+    println(s"Coefficients:")
+    lrModel2.coefficients.toArray.foreach(x => println(x))
+    println(s"Intercept: ${lrModel2.intercept}")
 
     val trainingSummary = lrModel2.summary
     println(s"numIterations: ${trainingSummary.totalIterations}")
@@ -142,15 +180,7 @@ object App {
     trainingSummary.residuals.show()
     println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
     println(s"r2: ${trainingSummary.r2}")
+  */
 
-    /*
-        println(s"Coefficients: ${lrModel.coefficients}")
-        println(s"Intercept: ${lrModel.intercept}")
-        val trainingSummary = lrModel.summary
-        println(s"numIterations: ${trainingSummary.totalIterations}")
-
-        lrModel.coefficients.toArray.foreach(x => println(x))
-          */
-    // validation measures
-}
+  }
 }
